51589e82417e3fb4b9e7444c30faecda9479ebd3
sergeyk <jsergeyk@gmail.com>
2019-06-27 01:46:51 +0300

    Добавил 1 комментарий

--- src/ua/itea/MyController.java
+++ src/ua/itea/MyController.java
@@ -78,6 +78,9 @@ public class MyController{
 		return pnlBrowser;
 	}
 	
+	/*
+	 * Изменение языка
+	 */
 	private void changeLang() {		
 		ResourceBundle messages = ResourceBundle.getBundle("Bundle",currentLocale);
 		System.out.println(pnlBrowser == null);
