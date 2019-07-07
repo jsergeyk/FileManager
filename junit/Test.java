b1bd45fb0aee52786da8492b41345a06747cc64b
sergeyk <jsergeyk@gmail.com>
2019-07-07 15:27:49 +0300

    Добавил Build.xml, поправил опечатку

---
+++ junit/Test.java
@@ -0,0 +1,18 @@
+package ua.itea;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Admin
+ */
+public class Test {
+	public Test() {
+		 
+	 }
+
+	@org.junit.Test
+	public void test() {
+		assertEquals(1l, 1l);
+	}
+
+}
